a0437c5a0e13524625def3873fa8404ce9b47145
Ziwei Yang <yangziwei@me.com>
2020-12-29 16:39:13 +0800

    Update FlashLoanReceiverBase.sol

--- FlashLoanReceiverBase.sol
+++ FlashLoanReceiverBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.6.12;
+pragma solidity 0.7.6;
 
 import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "Interfaces.sol";
 import { SafeERC20, SafeMath } from "Libraries.sol";
@@ -8,11 +8,13 @@ abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
-  ILendingPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
-  ILendingPool public immutable override LENDING_POOL;
+  ILendingPoolAddressesProvider internal _addressesProvider;
+  ILendingPool internal _lendingPool;
 
-  constructor(ILendingPoolAddressesProvider provider) public {
-    ADDRESSES_PROVIDER = provider;
-    LENDING_POOL = ILendingPool(provider.getLendingPool());
+  constructor(ILendingPoolAddressesProvider provider) {
+    _addressesProvider = provider;
+    _lendingPool = ILendingPool(ILendingPoolAddressesProvider(provider).getLendingPool());
   }
+
+  receive() external payable {}
 }
